db27215d895caa4b36e207d022dd67c6448f34aa
Nick <insherlolqq@gmail.com>
2021-12-23 21:47:58 +0300

    step 5

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,41 +1,39 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.Differ.Differ;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<String> {
-    @Parameters(index = "filepath1", description = "path to first file")
-    private Path filepath1;
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
 
-    @Parameters(index = "filepath2", description = "path to second file")
-    private Path filepath2;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format = "stylish";
 
     @Override
-    public String call() throws Exception { // your business logic goes here...
-        String fileContent1 = Files.readString(filepath1);
-        String fileContent2 = Files.readString(filepath2);
+    public Integer call() throws Exception {
+        String fileContent1 = Files.readString(Paths.get(filepath1));
+        String fileContent2 = Files.readString(Paths.get(filepath2));
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> mapOfFile1 = mapper.readValue(fileContent1, HashMap.class);
-        Map<String, String> mapOfFile2 = mapper.readValue(fileContent2, HashMap.class);
-//        byte[] fileContents = Files.readAllBytes(file.toPath());
-//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
-//        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
-        return fileContent1 + fileContent2;
+        Map<String, Object> mapOfFile1 = mapper.readValue(fileContent1, Map.class);
+        Map<String, Object> mapOfFile2 = mapper.readValue(fileContent2, Map.class);
+        String differ = Differ.generate(mapOfFile1, mapOfFile2);
+        return 0;
     }
 
     public static void main(String[] args) {
--- src/main/java/hexlet/code/Differ/Differ.java
+++ src/main/java/hexlet/code/Differ/Differ.java
@@ -1,7 +1,33 @@
-package hexlet.code;
+package hexlet.code.Differ;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Differ {
-    public static String generate(){
-        return "123";
+    public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
+        List<String> result = new ArrayList<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+        for (String key : keys) {
+            if (data1.containsKey(key) && data2.containsKey(key) && data1.get(key).equals(data2.get(key))) {
+                result.add("   " + key + ": " + data1.get(key));
+            }
+            if (!data2.containsKey(key) && data1.containsKey(key)) {
+                result.add(" - " + key + ": " + data1.get(key));
+            }
+            if (!data1.containsKey(key) && data2.containsKey(key)) {
+                result.add(" + " + key + ": " + data2.get(key));
+            }
+            if (data1.containsKey(key) && data2.containsKey(key) && !data1.get(key).equals(data2.get(key))) {
+                result.add(" - " + key + ": " + data1.get(key));
+                result.add(" + " + key + ": " + data2.get(key));
+            }
+        }
+        result.add(0, "{");
+        result.add(result.size(), "}");
+        String result1 = result.stream()
+                .collect(Collectors.joining("\n"));
+        System.out.println(result1);
+        return result1;
     }
 }
