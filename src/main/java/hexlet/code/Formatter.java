6f10428a77cd8a5109e55f1d4368539e8bac0017
Nick <insherlolqq@gmail.com>
2022-01-11 16:36:59 +0300

    cosmetic fix

--- src/main/java/hexlet/code/Formatter.java
+++ src/main/java/hexlet/code/Formatter.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.Formatters.Stylish;
 import hexlet.code.Formatters.Plain;
 import hexlet.code.Formatters.Json;
+
 import java.util.List;
 
 public final class Formatter {
